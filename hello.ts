1c848bdf47003ad47894daedcf50c57f64fa25c7
Denis <denis.subocz@gmail.com>
2023-04-12 14:01:23 +0200

    123

--- hello.ts
+++ hello.ts
@@ -1,9 +1,1 @@
-function powersOfTwo(n: number): number[] {
-    let acc: number[] = [];
-  
-    for (let i = 0; i <= n; i++) {
-      acc.push(Math.pow(2, i));
-    }
-    console.log(acc)
-    return acc;
-  }
+/*123*/

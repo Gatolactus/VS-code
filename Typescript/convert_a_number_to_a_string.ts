de3b7580930258d29e860716402701a28c4dd329
Denis <denis.subocz@gmail.com>
2023-05-17 21:40:35 +0200

    17.05.2023

--- Typescript/convert_a_number_to_a_string.ts
+++
@@ -1,8 +0,0 @@
-/*
-We need a function that can transform a number (integer) into a string.
-*/
-
-function numberToString(num: number): string {
-  const str:string = String(num);
-  return str;
-}
